e1e7e8cee1cafde780aaa8da3d31272573b347de
RocketFan <kubal123.tomczak@gmail.com>
2025-12-07 20:35:23 +0100

    feat: solve day 7 second star with recursion

--- day07/src/bin/day07-star1.rs
+++ day07/src/bin/day07-star1.rs
@@ -3,7 +3,7 @@ use std::path::Path;
 
 fn main() {
     let filepath = Path::new("day07/input.txt");
-    let (mut manifold, _) = read_tachyon_manifold_with_beam_root(filepath);
+    let (mut manifold, _) = read_tachyon_manifold_with_beam_position(filepath);
     println!("Tachyon manifold: {:?}", manifold);
     let splits = draw_beams_and_calculate_splits(&mut manifold);
     println!("Number of beam splits: {}", splits)
--- day07/src/bin/day07-star2.rs
+++ day07/src/bin/day07-star2.rs
@@ -1,50 +1,39 @@
 use day07::*;
-use std::path::Path;
+use std::{collections::HashMap, path::Path};
 
-fn count_possible_paths(node: &Node) -> usize {
-    if node.children.is_empty() {
+fn count_possible_paths(
+    manifold: &Vec<Vec<ManifoldField>>,
+    lookup: &mut HashMap<(usize, usize), usize>,
+    row: usize,
+    col: usize,
+) -> usize {
+    if row >= manifold.len() {
         return 1;
     }
 
-    let possible_paths = node
-        .children
-        .iter()
-        .map(|child| count_possible_paths(&*child.borrow()))
-        .sum();
-    possible_paths
+    if let Some(count) = lookup.get(&(row, col)) {
+        return *count;
+    }
+
+    match manifold[row][col] {
+        ManifoldField::Splitter => {
+            let left = count_possible_paths(manifold, lookup, row, col - 1);
+            let right = count_possible_paths(manifold, lookup, row, col + 1);
+
+            lookup.insert((row, col), left + right);
+
+            return left + right;
+        }
+        _ => count_possible_paths(manifold, lookup, row + 1, col),
+    }
 }
 
-// fn count_possible_paths(manifold: &Vec<Vec<ManifoldField>>, row: usize, col: usize) -> usize {
-//     let mut row = row;
-
-//     while row < manifold.len() {
-//         match manifold[row][col] {
-//             ManifoldField::Splitter => {
-//                 let left = count_possible_paths(manifold, row, col - 1);
-//                 let right = count_possible_paths(manifold, row, col + 1);
-
-//                 return left + right;
-//             }
-//             _ => row += 1,
-//         }
-//     }
-
-//     1
-// }
-
 fn main() {
-    let filepath = Path::new("day07/example.txt");
-    let (mut manifold, root) = read_tachyon_manifold_with_beam_root(filepath);
+    let filepath = Path::new("day07/input.txt");
+    let (mut manifold, beam_position) = read_tachyon_manifold_with_beam_position(filepath);
     println!("Tachyon manifold: {:?}", manifold);
     let _ = draw_beams_and_calculate_splits(&mut manifold);
     println!("Changed tachyon manifold: {:?}", manifold);
-    let possible_paths = count_possible_paths(&*root.borrow());
-    // let beam_position = manifold
-    //     .first()
-    //     .unwrap()
-    //     .iter()
-    //     .position(|x| x == ManifoldField::Beam)
-    //     .unwrap();
-    // let possible_paths = count_possible_paths(&manifold, 0, beam_position);
+    let possible_paths = count_possible_paths(&manifold, &mut HashMap::new(), 0, beam_position);
     println!("Number of possible paths: {}", possible_paths)
 }
